f51b035cae8b9bbca21e4cf42f06d77ccfd014c1
alexgphan <alexander.jl.phan@gmail.com>
2023-09-18 20:56:44 -0400

    Start of Tree Class.

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,18 @@
 public class Tree {
+    T root;
+    T subtree;
+
+    public void setRoot(T root) {
+        this.root = root;
+    }
+
+    public void setSubtree(T subtree) {
+        this.subtree = subtree;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+    static void __init__()
 }
